e56a97ddbf66d962c5639dd09f9537d94e3cecd0
Dean Tawonezvi <deantawonezvi1@gmail.com>
2021-04-25 11:56:01 +0200

    Adds mobile field to user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('mobile')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->enum('type',['Employee','Supervisor','Manager','Admin']);
             $table->string('department');
