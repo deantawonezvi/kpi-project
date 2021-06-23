bfb837174e244e64342cadde6ea6fa59c0becf1e
Dean Tawonezvi <deantawonezvi1@gmail.com>
2021-06-23 08:26:01 +0200

    Fixes sms service

--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -10,18 +10,13 @@ class SmsService
 {
     public static function sendSMSOtp($mobile, $message){
 
-        $headers = array(
-            'Accept'        => 'application/json',
-            'Authorization' => 'Basic R1RCVUNLUzE6R2V0YnVja3MyMDE4ISM=',
-        );
-        $client = new GuzzleHttp\Client(['verify'  => false,
-                                         'headers' => $headers]);
+        $client = new GuzzleHttp\Client(['verify'  => false]);
 
         $request_data = array('from' => 'GetBucks',
                               'to'   => $mobile,
                               'text' => $message);
 
-        $res = $client->post('https://api.infobip.com/sms/1/text/single', ['json' => $request_data]);
+        $res = $client->post('https://api.infobip.com/sms/1/text/single', ['json' => $request_data,'auth' => ['GTBUCKS1', 'Getbucks2018!#']]);
         Log::debug($res->getBody()->getContents());
 
         return json_decode($res->getBody()->getContents());
